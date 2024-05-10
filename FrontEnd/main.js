ef2611ee6e8bda1685f2d88625f4191ea07fdfb2
Nathalie Paroubek <nathio_games@orange.fr>
2024-05-10 18:23:59 +0200

    filter gallery functions and dynamic DOM display

--- FrontEnd/main.js
+++ FrontEnd/main.js
@@ -1,20 +1,23 @@
-/*  Declaration of the path to the API (will allow us to not retype it for each request) */
+//  Declaration of the path to the API (will allow us to not retype it for each request) 
 const backendHost = "http://localhost:5678/api"
 
+// Stores the API response for the "works" ressource 
 let worksRessource = [];
-let displayedWorkRessource = []
+// Stores the API content displayed in the DOM  
+let displayedWorksRessource = []
+// Stores the category names of the works 
 let categoryKeysArray = ["Tous"]
 
-/*  Function that gets the pictures and their titles from the API and insert them into the DOM  */
+//  Function that gets the data from the API and deserializes it
 async function getWorksApi() {
     try {
-        /*  Sends a request to the API for the ressource /works */
+        //  Sends a request to the API for the ressource /works
         let responsePictures = await fetch(`${backendHost}/works`)
 
-        /* Deserializes the response of the request (turns the response into a JS object ) */
+        // Deserializes the response of the request (turns the response into a JS object )
         let worksApiResponse = await responsePictures.json()
 
-        /*  Throws an error if the response is not an array */
+        //  Throws an error if the response is not an array 
         if (!Array.isArray(worksApiResponse)) {
             throw new Error("Unexpected response")
         }
@@ -28,24 +31,25 @@ async function getWorksApi() {
     }
 }
 
-function displayPictures() {
+//  Renders the gallery content with the API response
+function renderGalleryContent() {
 
-    /*  Get the DOM element where the pictures will be inserted */
+    //  Get the DOM element where the pictures will be inserted 
     const pictureContainer = document.querySelector(".gallery")
+    //  Removes the content of the gallery
+    pictureContainer.innerHTML = ""
 
+    // Creates a new DOM elements for each picture and each title 
+    for (let i = 0; i < displayedWorksRessource.length; i++) {
 
-    /* Creates a new DOM elements for each picture and each title */
-    for (let i = 0; i < worksRessource.length; i++) {
-
-        /* console.log(worksRessource[i]) */
         let newFigure = document.createElement("figure")
         let newPicturElement = document.createElement("img")
-        newPicturElement.src = worksRessource[i].imageUrl
-        newPicturElement.alt = worksRessource[i].title
+        newPicturElement.src = displayedWorksRessource[i].imageUrl
+        newPicturElement.alt = displayedWorksRessource[i].title
         let newFigcaption = document.createElement("figcaption")
-        newFigcaption.innerText = worksRessource[i].title
+        newFigcaption.innerText = displayedWorksRessource[i].title
 
-        /*  Appends the new elements to the DOM */
+        //  Appends the new elements to the DOM
         newFigure.appendChild(newPicturElement)
         newFigure.appendChild(newFigcaption)
         pictureContainer.appendChild(newFigure)
@@ -68,46 +72,85 @@ function filterPictures(searchCategory) {
 }
 */
 
-function filterPictures(searchCategory) {
+//  Gets the works from the worksRessource according to their category name
+function filterCategory(selectedCategory) {
 
-    return worksRessource.filter((element) => {
-        return element.category.name === searchCategory || searchCategory === "Tous"
-    })
+    // Returns all the works from the worksResource if the selectedCategory is "Tous"
+    if (selectedCategory === "Tous") {
+        return worksRessource
+    }
+    // Returns the works belonging to the selectedCategory
+    else {
+        return worksRessource.filter((element) => {
+            // return true || false => return true
+            // return true && false => return false
+            /* return element.category.name === selectedCategory || selectedCategory === "Tous" */
+            return element.category.name === selectedCategory
+        })
+    }
 }
 
-
-// call the function 
-
-console.log(new Date().getMilliseconds())
-
-getWorksApi().then(() => {
-    console.log(new Date().getMilliseconds())
-    displayPictures() /* Call filter before with "Tous" */
-    extractKeys()
-    console.log(categoryKeysArray)
-    console.log(filterPictures(categoryKeysArray[3]))
-    console.log(worksRessource)
-})
-
-
+/* 
+Get the key "name" from the worksRessource category array and adds the category 
+names into the categoryKeyArray
+*/
 function extractKeys() {
 
     for (let i = 0; i < worksRessource.length; i++) {
         if (!categoryKeysArray.includes(worksRessource[i].category.name)) {
-
             categoryKeysArray.push(worksRessource[i].category.name)
-
         }
     }
-
 }
 
-/* 
 
-Create a function that displays the keys from the categoryKeysArray[] in the DOM
-Create another function that is exectuted onclick of one of they keys of the categoryKeysArray
-that replaces the "displayedWorksRessource" with the result of "filteredPictures(selectedKeys)"
-Change function displayPictures into display displayedWorkRessource
+function displayCategoryKeys() {
 
-*/
+    //  Gets the DOM element where the filter menu will be inserted
+    const filterMenu = document.querySelector(".filter")
+
+    /*  Creates the filter buttons and gives them a class, an attribute
+     according to their category names and calls the focusCategory function  */
+    for (let i = 0; i < categoryKeysArray.length; i++) {
+
+        let categoryTitle = document.createElement("button")
+        categoryTitle.classList.add("category-button")
+        categoryTitle.setAttribute("onclick", `focusCategory("${categoryKeysArray[i]}")`)
+        categoryTitle.setAttribute("category-key", categoryKeysArray[i])
+        categoryTitle.innerText = categoryKeysArray[i]
+
+        //  Appends the new elements in the DOM
+        filterMenu.appendChild(categoryTitle)
+    }
+}
+
+//
+function focusCategory(selectedCategory) {
+    //  Copies the filtered works from worksRessource thanks to the filterCategory function
+    displayedWorksRessource = filterCategory(selectedCategory)
+    //  Renders the gallery content
+    renderGalleryContent()
+
+    //  Gets the filter buttons and adds the focus style to the selected button
+    const buttons = document.getElementsByClassName("category-button")
+    for (let i = 0; i < buttons.length; i++) {
+
+        if (buttons[i].getAttribute("category-key") === selectedCategory) {
+            buttons[i].classList.add("filter-focus")
+        } else {
+            buttons[i].classList.remove("filter-focus")
+        }
+    }
+}
+
+
+// Calls the functions that need the API response to get executed
+function processResponseApi() {
+    extractKeys()
+    displayCategoryKeys()
+    focusCategory("Tous")
+}
+
+// Executes the function calls after receiving the response of the API 
+getWorksApi().then(processResponseApi)
 
